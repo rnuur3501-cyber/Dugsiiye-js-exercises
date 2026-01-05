0e9faffbf1624e162ffdfe5e81328428091e1b79
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-05 10:22:27 +0300

    submit exercise 4

---
+++ exercise 4/main.js
@@ -0,0 +1,22 @@
+// function basics
+
+function add(x,y) {
+    return x+y ;
+}
+
+console.log(add(30,25)) ;
+console.log(add(20,45)) ;
+console.log(add(71,16)) ;
+console.log(add(2,65)) ;
+
+
+// function expression 
+
+let calculate = function(x, y) {
+  console.log("Their total is " + (x + y));
+}
+
+calculate(10, 10);
+calculate(2, 23);
+calculate(10, 30);
+
