1ad4fa04d841539119ce1d81ee1072e2cb88b7ac
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-05 19:19:48 +0300

    submit the exercise 7

---
+++ exercise7/main.js
@@ -0,0 +1,20 @@
+
+
+
+// common array method exercise
+
+let colors = ["purple", "pink", "black"] ;
+
+console.log(colors)
+
+console.log("before push")
+
+colors.push("white")
+
+console.log(colors)
+
+console.log("before unshift")
+
+colors.shift()
+
+console.log(colors)
