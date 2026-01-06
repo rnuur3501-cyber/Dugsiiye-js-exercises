d05f9c4a0a326f261a1679b749560d18bd7d46a9
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-06 19:35:33 +0300

    submit the exercise 10

---
+++ exercise10/main.js
@@ -0,0 +1,24 @@
+// arrays of objects
+
+let library = [
+   {
+   title:"atomic habitas",
+   author:"James Clear",
+   year:"October 16, 2018",
+   },
+   {
+    title:"waaku salaame sxb",
+    author:"Adham Sharqawi",
+    year:"2023-2024.",
+   },
+   {
+    title:"mucjisada aroortii",
+    author: "Hal Elrod",
+    year:"2012",
+   }
+
+]
+console.log(library[0].title);
+console.log(library[1].author);
+
+
