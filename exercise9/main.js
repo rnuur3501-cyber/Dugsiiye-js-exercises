c0a8573a92738f2add4de77eed4781770c15e124
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-06 12:32:30 +0300

    submit the exercise 9

---
+++ exercise9/main.js
@@ -0,0 +1,15 @@
+let car = {
+
+    make: "Bugatti",
+    model: "La Voiture Noire",
+    year: 2025,
+    start : () => {
+       console.log("the car has started.")
+    }
+   
+}
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+car.start()
