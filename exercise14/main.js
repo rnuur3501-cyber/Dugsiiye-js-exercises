964b22257935e6f2abbbd6094c01b88a2def33b9
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-06 23:47:43 +0300

    submit the exercise 14

---
+++ exercise14/main.js
@@ -0,0 +1,19 @@
+// // do while loop
+
+// let number;
+//        number=prompt("enter your name");
+// do {
+    
+// } while (number < 10)
+
+// console.log("Thank you! You entered:");
+
+
+let number;
+
+do {
+    number = prompt("Enter a number greater than 10:");
+    // number = Number(number); // convert the input to a number
+} while (number <= 10);
+
+console.log("Thank you! You entered ");
