db09506f8d0b786abbdb4d78af60045f244e44b7
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-10 15:25:24 +0300

    submit the exercise 17

---
+++ exercise17/main.js
@@ -0,0 +1,13 @@
+
+
+let tempreture = 25;
+
+if ( tempreture <= 0) {
+   console.log("very cold")
+}else if ( tempreture < 15 ) {
+   console.log("cold")
+}else if ( tempreture < 25 ) {
+   console.log("warm")
+}else {
+   console.log("hot")
+}
