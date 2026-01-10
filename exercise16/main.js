c66f770edad744f5c469557a26b067e8330f95ec
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-01-10 15:05:56 +0300

    submit the exercise 16

---
+++ exercise16/main.js
@@ -0,0 +1,22 @@
+// if else statement
+
+let score = 50 ;
+
+if (score >= 50) {
+   console.log("you are passed");
+}else {
+    console.log("you are failed");
+}
+
+
+
+
+// kanne waa ayadoo userka laweeiinaayo
+
+
+// let score=Number(prompt("enter your score"))
+// if (score >= 50) {
+//    console.log("you are passed");
+// }else {
+//     console.log("you are failed");
+// }
