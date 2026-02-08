8b7013078e23b3bb1ccc9163cbbd0b0a4f29199e
Rahma Nor Abdirahman <rnuur3501@gmail.com>
2026-02-08 15:35:21 +0300

    submit the exercise 32

---
+++ exercise32/main.js
@@ -0,0 +1,7 @@
+// querySelector and querySelectorAll
+
+const head = document.querySelector("#head");
+console.log(head);
+
+const paragraph = document.querySelectorAll(".paragraph")[2];
+console.log(paragraph);
